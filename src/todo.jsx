4face5e211fb77b5331e313deff86d102695e0d2
Nikhil <devsotnik@gmail.com>
2022-08-30 23:33:59 +0530

    final touch to todo list basic

--- src/todo.jsx
+++ src/todo.jsx
@@ -5,58 +5,65 @@ function Todo(){
     let [task,setTask] = useState([])
     let [input,setInput] = useState("")
     let [edit,setEdit] = useState(-1)
+    let [edittxt,setEditxt] = useState("")
 
-    let AddTask=()=>{
+    let AddTask=(e)=>{
+        e.preventDefault()
         setTask([...task,input])
+        setInput("")
     }
     let DeleteTask=(index)=>{
         task.splice(index,1)
-        console.log(task)
+        setInput("")
+        setTask([...task])
     }
     let EditTask=(index)=>{
         setEdit(index)
         let i= task[index]
-        setInput(i)
-        // task.splice(index,1)
+        // setInput(i)
+        setEditxt(i)
     }
     let Save=(index)=>{
         setEdit(-1)
-        task.splice(index,1,input)
-        // setTask([...task,input])
+        // let editstr = task[index]
+        // setEditxt(editstr)
+        task.splice(index,1,edittxt)
+        setInput(null)
     }
-    // useEffect(()=>{
-    //    console.log(task)
-    // },[task,input])
+   
+   
     return(
         <>
-            <div className="App">
+            <div className="main">
             <h1>TODO List</h1>
-            <input type="text" value={input} onChange={(e)=>{setInput(e.target.value)}}/>
-            <button type="button" onClick={AddTask}>Add List</button>
-            <div   style={{width:"300px",margin:"auto"}}>
+            <form onSubmit={AddTask}>
+            <input type="text" value={input} onChange={(e)=>{setInput(e.target.value)}}  required/>&nbsp;&nbsp;
+            <button type="submit" >Add List</button>
+            </form>
+            <div className="list">
             {
                 task.length > 0 &&
                 task.map((item,index)=>{
                     return(
                         edit === index ?
                         <>
-                        <div key={index} style={{display:"flex",justifyContent:"space-between"}}>
-                        <div>
-                        <input type="text" value={input} onChange={(e)=>{setInput(e.target.value)}}></input>
-                        </div>  
-                        <div>
-                        <button type="button" onClick={()=>Save(index)}>Save</button>
-                        <button type="button" onClick={()=>DeleteTask(index)} >Delete</button>
-                        </div>
+                        <div key={index} className="inner">
+                            <div>
+                            <input type="text" value={edittxt}  onChange={(e)=>{setEditxt(e.target.value)}}></input>
+                            </div>  
+                            <div>
+                            <button type="button" id="save" onClick={()=>Save(index)}>Save</button>&nbsp;&nbsp;
+                            <button type="button" onClick={()=>DeleteTask(index)} >Delete</button>
+                            </div>
                          </div>
                          </>
                         :
-                        <div key={index} style={{display:"flex",justifyContent:"space-between"}}>
+                        <div key={index} className="inner">
                         <div>
                         <label>{item}</label>
                         </div>
                         <div>
-                        <button type="button" onClick={()=>EditTask(index)}>Edit</button>
+                        <button type="button" onClick={()=>EditTask(index)}>Edit</button>&nbsp;&nbsp;
                         <button type="button" onClick={()=>DeleteTask(index)} >Delete</button>
                         </div>
                         </div>
